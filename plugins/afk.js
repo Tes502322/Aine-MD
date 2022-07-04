79ef3f7761bcc7568b9d0be0321d9125e436d4a1
Nonhoman4 <106759651+Nonhoman4@users.noreply.github.com>
2022-07-04 10:55:06 +0800

    afk.js

--- plugins/afk.js
+++ plugins/afk.js
@@ -1,4 +1,4 @@
-let handler = async (m, { text }) => {
+/*let handler = async (m, { text }) => {
   let user = global.db.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
@@ -12,3 +12,4 @@ handler.command = /^afk$/i
 handler.limit = true
 
 module.exports = handler
+*/
